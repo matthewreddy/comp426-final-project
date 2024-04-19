54db6cf9da4bd867819bf1685ffc08bd9e1e30e3
matthewreddy <mjreddy@unc.edu>
2024-04-18 23:10:10 -0400

    Add stylesheet

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ let parent = document.getElementById("main");
 
 let header = document.createElement("h1");
 header.textContent = "This project is a dub.";
+header.classList.add("highlight");
 
 parent.append(header);
